48d2be8e6a386dfb2e6aec5a68417dc2c6812603
matteocitterio <72971538+matteocitterio@users.noreply.github.com>
2026-04-23 22:22:15 +0200

    Create content.js

---
+++ data/content.js
@@ -0,0 +1,10 @@
+/* ============================================
+   DATI DEL SITO
+   Modifica questo file per aggiornare:
+   - data del matrimonio (countdown)
+   - link Google Form RSVP
+   ============================================ */
+window.WEDDING = {
+    weddingDate: "2026-09-26T14:30:00",
+    rsvpUrl: "https://docs.google.com/forms/d/e/1FAIpQLSeSbS-p6_9yFn-WmZgjH7otbLdoBrWzRb05O3tWbp7UGI401A/viewform"
+};
